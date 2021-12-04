4e1079c8e587e185e77e9a1526c5a690ac2f77dd
aaqilrk <aaqilrk@gmail.com>
2021-12-04 19:32:20 +0530

    render all books in homepage

--- controllers/homeController.js
+++ controllers/homeController.js
@@ -1,5 +1,44 @@
+const {
+    Op
+} = require("sequelize");
+const Book = require('../models/book');
+const BookCategory = require('../models/category');
+const BookAuthor = require('../models/author');
+
 module.exports.home = (req, res) => {
-    return res.render('home', {
-        title: "Home"
-    });
+    Book.findAll({
+            attributes: ['ISBN', 'title', 'price', 'noOfPages', 'discount', 'type', 'publisher'],
+            raw: true
+        })
+        .then(async books => {
+            for (let book of books) {
+                await BookAuthor.findAll({
+                        attributes: ['name'],
+                        where: {
+                            ISBN: book.ISBN
+                        },
+                        raw: true
+                    })
+                    .then(authorList => book.authorList = authorList)
+                    .catch(err => console.log(err));
+
+                await BookCategory.findAll({
+                        attributes: ['name'],
+                        where: {
+                            ISBN: book.ISBN
+                        },
+                        raw: true
+                    })
+                    .then(categoryList => book.categoryList = categoryList)
+                    .catch(err => console.log(err));
+            }
+            return books;
+        })
+        .then(books => {
+            return res.render('home', {
+                title: "Home",
+                books: books
+            });
+        })
+        .catch(err => console.log(err));
 };
