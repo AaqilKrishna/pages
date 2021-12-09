0abb689ac3b6a5d07822e05ca7716ae7b12e9aa3
aaqilrk <aaqilrk@gmail.com>
2021-12-09 07:55:16 +0530

    display seletected books in cart

--- controllers/cartController.js
+++ controllers/cartController.js
@@ -2,11 +2,49 @@ const {
     Op
 } = require("sequelize");
 const Cart = require('../models/cart');
+const Book = require('../models/book');
+const BookAuthor = require('../models/author');
 
 module.exports.cart = (req, res) => {
-    return res.render('cart', {
-        title: "Cart"
-    });
+    Cart.findAll({
+            attributes: ['ISBN', 'quantity'],
+            where: {
+                customerId: req.user.id
+            },
+            raw: true
+        })
+        .then(async books => {
+            for (let book of books) {
+                await Book.findByPk(book.ISBN, {
+                        attributes: ['title', 'price', 'discount', 'type', 'image'],
+                        raw: true
+                    })
+                    .then(bookData => {
+                        book.title = bookData.title,
+                            book.price = bookData.price,
+                            book.discount = bookData.discount,
+                            book.type = bookData.type,
+                            book.image = bookData.image
+                    })
+                    .catch(err => console.log(err));
+
+                await BookAuthor.findAll({
+                        attributes: ['name'],
+                        where: {
+                            ISBN: book.ISBN
+                        },
+                        raw: true
+                    })
+                    .then(authorList => book.authorList = authorList)
+                    .catch(err => console.log(err));
+            }
+
+            return res.render('cart', {
+                title: "Cart",
+                books: books
+            });
+        })
+        .catch(err => console.log(err));
 }
 
 module.exports.add = (req, res) => {
